ccac57849c4c3ecc4280e1fd1efec77e205ece3f
MunavvarSinan <munavvarsinan8@gmail.com>
2023-05-20 11:27:13 +0530

    implemented tweet crud operations

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,30 +1,69 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const tweetRoutes = express.Router();
-
+const prisma = new PrismaClient();
 //create tweet
-tweetRoutes.post('/', (req, res) => {
+tweetRoutes.post('/', async (req, res) => {
+    const { content, image, userId } = req.body;
+    try {
+        const result = await prisma.tweet.create({
+            data: {
+                content,
+                image,
+                userId
+            }
+        })
+        res.status(200).json(result);
+
+    } catch (error) {
+        res.status(400).json({ error: "Failed to create tweet" })
+    }
 
 })
 
 // list tweets
-tweetRoutes.get('/', (req, res) => {
-
+tweetRoutes.get('/', async (req, res) => {
+    const tweets = await prisma.tweet.findMany();
+    res.json(tweets);
 })
 
 // Get one tweet
-tweetRoutes.get('/:id', (req, res) => {
+tweetRoutes.get('/:id', async (req, res) => {
     const { id } = req.params;
+    try {
+        const result = await prisma.tweet.findUnique({ where: { id: Number(id) } });
+        if (!result) return res.status(404).json({ error: "Tweet not found" });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).json({ error: "Failed to get tweet" })
+    }
+
 })
 
 //update tweet
-tweetRoutes.put('/:id', (req, res) => {
+tweetRoutes.put('/:id', async (req, res) => {
     const { id } = req.params;
+    const { content, image } = req.body;
+    try {
+        const result = await prisma.tweet.update({
+            where: { id: Number(id) },
+            data: {
+                content,
+                image
+            }
+        })
+        res.status(200).json({ message: "Tweet updated successfully", result });
+    } catch (error) {
+        res.status(400).json({ error: "Failed to update tweet" })
+    }
 })
 
 //delete tweet
-tweetRoutes.delete('/:id', (req, res) => {
+tweetRoutes.delete('/:id', async (req, res) => {
     const { id } = req.params;
+    await prisma.tweet.delete({ where: { id: Number(id) } });
+    res.status(200).json({ message: "Tweet deleted successfully" });
 })
 
 export default tweetRoutes;
